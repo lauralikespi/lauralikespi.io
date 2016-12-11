83dae0cfb1ae1517877a61720378a809a531d65d
lauragemmell1 <laura.gemmell@worldpay.com>
2016-12-11 21:41:16 +0000

    Changing Index
    Linking in Basic Javascript file
    Redesign - smaller pictures, more sections

--- js/myjQuery.js
+++ js/myjQuery.js
@@ -1,29 +1,32 @@
 $(document).ready(function(){
 	$('#fadingPic1 p').hide();
 	$('#fadingPic1 img').mouseover(function(){
-		$('#fadingPic1 p').show();
+		$('#fadingPic1 .fadingText').show();
 		$('#fadingPic1 img').stop().fadeTo("slow",0.6);
 	});
 	$('#fadingPic1 img').mouseleave(function(){
-		$('#fadingPic1 p').hide();
+		$('#fadingPic1 .fadingText').hide();
 		$('#fadingPic1 img').stop().fadeTo("slow",1);
+	});
+	$('#fadingPic1 img').click(function(){
+		$('#fadingPic1 .clickText').show();
 	});
 	$('#fadingPic2 p').hide();
 	$('#fadingPic2 img').hover(function(){
-		$('#fadingPic2 p').show();
+		$('#fadingPic2 .fadingText').show();
 		$('#fadingPic2 img').stop().fadeTo("slow",0.6);
 	});
 	$('#fadingPic2 img').mouseleave(function(){
-		$('#fadingPic2 p').hide();
+		$('#fadingPic2 .fadingText').hide();
 		$('#fadingPic2 img').stop().fadeTo("slow",1);
 	});
 	$('#fadingPic3 p').hide();
 	$('#fadingPic3 img').hover(function(){
-		$('#fadingPic3 p').show();
+		$('#fadingPic3 .fadingText').show();
 		$('#fadingPic3 img').stop().fadeTo("slow",0.6);
 	});
 	$('#fadingPic3 img').mouseleave(function(){
-		$('#fadingPic3 p').hide();
+		$('#fadingPic3 .fadingText').hide();
 		$('#fadingPic3 img').stop().fadeTo("slow",1);
 	});
 	$('.navigationBar li').mouseover(function(){
