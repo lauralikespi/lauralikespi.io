e7ad5b1ddbf3d893dd3fb5062f38572d7ed74655
lauralikespi <laura.gemmell012@gmail.com>
2018-04-25 07:41:45 +0100

    blah

---
+++ js/xmp.js
@@ -0,0 +1,6 @@
+var stuff='<xmp'+'><h1> Create your own sign! </h1>
+					<p> £5 per letter for signs less than 10 letters </p>
+					<p> £3.50 per letter for signs greater than 10 letters </p>
+					<p> Shipping is £7 </p>
+					<br></xmp>';
+document.getElementById("x").innerHTML=stuff;
