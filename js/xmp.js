d5453a9c2ae61e7aab92066d7d30acba0118d651
lauralikespi <laura.gemmell012@gmail.com>
2018-05-23 20:08:10 +0100

    new design

--- js/xmp.js
+++
@@ -1,6 +0,0 @@
-var stuff='<xmp'+'><h1> Create your own sign! </h1>
-					<p> £5 per letter for signs less than 10 letters </p>
-					<p> £3.50 per letter for signs greater than 10 letters </p>
-					<p> Shipping is £7 </p>
-					<br></xmp>';
-document.getElementById("x").innerHTML=stuff;
