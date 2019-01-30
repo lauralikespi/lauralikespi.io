85c3ab7e005eee497cac8ae15744c094e5c78d0d
lauralikespi <laura.gemmell012@gmail.com>
2019-01-30 20:27:18 +0000

    upcoming events added

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+var workExp = function(){
+  document.getElementById("corp-work-exp-feb-19").style.display = "block";
+  document.getElementById("potato-feb-19").style.display = "none";
+};
+
+var potato = function(){
+  document.getElementById("corp-work-exp-feb-19").style.display = "none";
+  document.getElementById("potato-feb-19").style.display = "block";
+};
