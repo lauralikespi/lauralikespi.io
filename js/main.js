69f941464e20a746deab87451d4c10926c2da132
lauralikespi <laura.gemmell012@gmail.com>
2019-02-09 11:58:30 +0000

    Updated events to add Paddle, fixed mobile Index (no longer only takes up half the page), redesigned mobile Upcoming Events

--- js/main.js
+++ js/main.js
@@ -8,9 +8,17 @@ navBarToggle.addEventListener('click', function () {
 var workExp = function(){
   document.getElementById("corp-work-exp-feb-19").style.display = "block";
   document.getElementById("potato-feb-19").style.display = "none";
+  document.getElementById("paddle-feb-19").style.display = "none";
 };
 
 var potato = function(){
   document.getElementById("corp-work-exp-feb-19").style.display = "none";
   document.getElementById("potato-feb-19").style.display = "block";
+  document.getElementById("paddle-feb-19").style.display = "none";
+};
+
+var paddle = function(){
+  document.getElementById("corp-work-exp-feb-19").style.display = "none";
+  document.getElementById("potato-feb-19").style.display = "none";
+  document.getElementById("paddle-feb-19").style.display = "block";
 };
