38c9e3529778ebed201618a30b6eec9268a45e98
Laura Gemmell <lg17706@bristol.ac.uk>
2020-02-25 16:41:51 +0000

    simple landing page with social media buttons and a blurb until the redesign

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-// document.getElementById("rightClickInspect").style.display = "none";
-// document.getElementById("dockSide").style.display = "none";
-// document.getElementById("toolbar").style.display = "none";
+// let mainNav = document.getElementById('js-menu');
+// let navBarToggle = document.getElementById('js-navbar-toggle');
+// // document.getElementById("rightClickInspect").style.display = "none";
+// // document.getElementById("dockSide").style.display = "none";
+// // document.getElementById("toolbar").style.display = "none";
 
-navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
-});
+// navBarToggle.addEventListener('click', function () {
+//   mainNav.classList.toggle('active');
+// });
 
-var workExp = function(){
-  document.getElementById("corp-work-exp-feb-19").style.display = "block";
-  document.getElementById("potato-feb-19").style.display = "none";
-  document.getElementById("paddle-feb-19").style.display = "none";
-};
+// var workExp = function(){
+//   document.getElementById("corp-work-exp-feb-19").style.display = "block";
+//   document.getElementById("potato-feb-19").style.display = "none";
+//   document.getElementById("paddle-feb-19").style.display = "none";
+// };
 
-var potato = function(){
-  document.getElementById("corp-work-exp-feb-19").style.display = "none";
-  document.getElementById("potato-feb-19").style.display = "block";
-  document.getElementById("paddle-feb-19").style.display = "none";
-};
+// var potato = function(){
+//   document.getElementById("corp-work-exp-feb-19").style.display = "none";
+//   document.getElementById("potato-feb-19").style.display = "block";
+//   document.getElementById("paddle-feb-19").style.display = "none";
+// };
 
-var paddle = function(){
-  document.getElementById("corp-work-exp-feb-19").style.display = "none";
-  document.getElementById("potato-feb-19").style.display = "none";
-  document.getElementById("paddle-feb-19").style.display = "block";
-};
+// var paddle = function(){
+//   document.getElementById("corp-work-exp-feb-19").style.display = "none";
+//   document.getElementById("potato-feb-19").style.display = "none";
+//   document.getElementById("paddle-feb-19").style.display = "block";
+// };
 
 // var rightClickInspectButton = function(){
 //   var x = document.getElementById("rightClickInspect");
